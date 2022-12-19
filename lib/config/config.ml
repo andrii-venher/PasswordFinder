90400a3731667afe51d7fddddf56f2bb050e5928
Andrii Venher <vengerandr@gmail.com>
2022-12-19 21:43:56 +0100

    Move password settings to config

--- lib/config/config.ml
+++ lib/config/config.ml
@@ -6,7 +6,7 @@ let filename =
   with _ -> 
     match Sys.getenv_opt "FILENAME" with
     | None -> "passwords.txt"
-    | Some(filename) -> filename
+    | Some filename -> filename
 
 let num_domains = 
   try
@@ -14,9 +14,29 @@ let num_domains =
   with _ -> 
     match Sys.getenv_opt "NUM_DOMAINS" with
     | None -> 8
-    | Some(num_domains) -> int_of_string num_domains
+    | Some num_domains -> int_of_string num_domains
 
 let debug_logs =
   match Sys.getenv_opt "DEBUG_LOGS" with
   | None -> false
-  | Some(num_domains) -> bool_of_string num_domains
+  | Some num_domains -> bool_of_string num_domains
+
+let password_length_min =
+  match Sys.getenv_opt "PASSWORD_LENGTH_MIN" with
+  | None -> 3
+  | Some length -> int_of_string length
+
+let password_length_max =
+  match Sys.getenv_opt "PASSWORD_LENGTH_MAX" with
+  | None -> 6
+  | Some length -> int_of_string length
+
+let password_smallest_letter =
+  match Sys.getenv_opt "PASSWORD_SMALLEST_LETTER" with
+  | None -> 'a'
+  | Some str -> String.get str 0
+
+let password_greatest_letter =
+  match Sys.getenv_opt "PASSWORD_GREATEST_LETTER" with
+  | None -> 'z'
+  | Some str -> String.get str 0
--- lib/pipeline/scheduler.ml
+++ lib/pipeline/scheduler.ml
@@ -19,7 +19,12 @@ let run_worker_groups_async pool worker_groups =
   run_groups worker_groups
 
 let run_parallel pool hashes out_ch =
-  let worker_blueprints = Builder.create_worker_blueprints 3 6 'a' 'z' hashes out_ch in
+  let worker_blueprints = Builder.create_worker_blueprints 
+    Config.password_length_min 
+    Config.password_length_max 
+    Config.password_smallest_letter 
+    Config.password_greatest_letter 
+    hashes out_ch in
   let in_chs = List.map Builder.get_in_ch worker_blueprints in
   let worker_groups = Builder.distribute_blueprints worker_blueprints (Config.num_domains - 1) in
   Io.log_debug (fun () -> Printf.printf "%s" (Builder.string_of_worker_distribution worker_groups));
